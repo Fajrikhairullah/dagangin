884afa2caa00704eda651bfef2bcebab840fda56
fajri khairullah <fajri.khairullah@gmail.com>
2020-07-09 11:41:05 +0700

    Initial Dagangin

---
+++ src/main/java/com/restapi/dagangin/DaganginApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.dagangin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaganginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaganginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restapi/dagangin/DaganginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.dagangin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaganginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
